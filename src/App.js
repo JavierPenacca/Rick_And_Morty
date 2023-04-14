100a0b3005ebb41dee9994332d534065a261aec9
JavierPenacca <javier.penacca@gmail.com>
2023-04-14 18:28:19 +0100

    ModifiRM

--- src/App.js
+++ src/App.js
@@ -1,24 +1,68 @@
-import './App.css';
-import Card from './components/Card.jsx';
-import Cards from './components/Cards.jsx';
-import SearchBar from './components/SearchBar.jsx';
-import characters, { Rick } from './data.js';
+import "./styles/App/app.css";
+import Cards from './components/Cards/Cards.jsx';
+import Nav from './components/Nav/Nav';
+import About from './components/About/About';
+import Detail from './components/Detail/Detail';
+import Form from "./components/Form/Form";
+import Favorites from "./components/Favorites/Favorites";
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+import { Routes, Route, useLocation, useNavigate } from 'react-router-dom';
+
+const URL_BASE = 'https://be-a-rym.up.railway.app/api/character';
+const API_KEY = 'cbff5e5cf15f.692e74f28c46ce43011d';
+
+const email = 'dai@gmail.com';
+const password = '123asd';
 
 function App() {
+   const location = useLocation();
+   const navigate = useNavigate();
+   const [characters, setCharacters] = useState([]);
+   const [access, setAccess] = useState(false);
+
+   const login = (userData) => {
+      if(userData.email === email && userData.password === password){
+         setAccess(true);
+         navigate('/home');
+      }
+   }
+
+   useEffect(() => {
+      !access && navigate('/')
+   }, [access])
+
+   const onSearch = (id) => {
+      axios(`${URL_BASE}/${id}?key=${API_KEY}`)
+      .then(response => response.data)
+      .then((data) => {
+         if (data.name) {
+            setCharacters((oldChars) => [...oldChars, data]);
+         } else {
+            window.alert('¡No hay personajes con este ID!');
+         }
+      });
+   }
+
+   const onClose = (id) => {
+      const charactersFiltered = characters.filter(character => character.id !== id)
+      setCharacters(charactersFiltered)
+   }
+
    return (
       <div className='App'>
-         <SearchBar onSearch={(characterID) => window.alert(characterID)} />
-         <Cards characters={characters} />
-         <Card
-            id={Rick.id}
-            name={Rick.name}
-            status={Rick.status}
-            species={Rick.species}
-            gender={Rick.gender}
-            origin={Rick.origin.name}
-            image={Rick.image}
-            onClose={() => window.alert('Emulamos que se cierra la card')}
-         />
+         {
+            location.pathname !== '/' && <Nav onSearch={onSearch} setAccess={setAccess} />
+         }
+         
+         <Routes>
+            <Route path='/' element={<Form login={login}/>} />
+            <Route path='/home' element={ <Cards characters={characters} onClose={onClose}/> }/>
+            <Route path='/about' element={<About/>} />
+            <Route path='/detail/:id' element={<Detail/>} />
+            <Route path='/favorites' element={<Favorites/>} />
+         </Routes>
+        
       </div>
    );
 }
